d57fea9803db74b7f8b3c0f7f2152a138fb0bc13
adityashankar80 <adityashankar80@gmail.com>
2020-02-03 14:14:08 +0530

    Update variables.tf

--- 04-Terraform/03-Ansible-Deploy/variables.tf
+++ 04-Terraform/03-Ansible-Deploy/variables.tf
@@ -1,15 +1,15 @@
 variable "key_name" {
-  default = "devops-my-aws-keypair"
+  default = "aws_301"
 }
 
 variable "pvt_key" {
-  default = "/root/.ssh/amit-personal-aws.pem"
+  default = "/root/.ssh/awskey.pem"
 }
 
 variable "us-east-zones" {
-  default = ["us-east-1a", "us-east-1b"]
+  default = ["us-east-2b"]
 }
 
 variable "sg-id" {
-  default = "sg-00c3c981ea2a2ad80"
+  default = "sg-035b4534b55ffa3ef"
 }
